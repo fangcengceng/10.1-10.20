4ac95f440b00274e1b1ad56e26cd8473c8fcf8b8
CengCeng.Fang <fangcengceng@bjxmail.com>
2017-12-12 10:42:20 +0800

    头疼

---
+++ test/test/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by bjx on 2017/12/12.
+//  Copyright © 2017年 fcc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+//a
+//    sd
+//    f
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ test/test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by bjx on 2017/12/12.
+//  Copyright © 2017年 fcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
